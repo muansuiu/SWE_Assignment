6e9f4709c928dff4e63c616fb1a5a3b67d420d67
javatcodein <mbiswas173036@bscse.uiu.ac.bd>
2022-01-08 15:25:52 +0600

    project update

--- MyFancyJavaProject.java
+++ MyFancyJavaProject.java
@@ -10,10 +10,20 @@ public class MyFancyJavaProject {
         this.price = price;
     }
 
+  MyProject(String pcModel,String pcBrand, double price)
+    {
+        this.pcModel = pcModel;
+        this.pcBrand = pcBrand;
+        this.price = price;
+    }
+
     public static void main(String[] args) {
 
         MyFancyJavaProject newPhone = new MyFancyJavaProject("Galaxy S21 Ultra", "Samsung",110000);
         System.out.println(newPhone.phoneModel + " " + newPhone.phoneBrand + " "+ newPhone.price);
+
+        MyFancyJavaProject newPC = new MyProject("thinkpad_genaration_5", "Lenovo",500000);
+        System.out.println(newPC.pcModel + " " + newPC.pcBrand + " "+ newPC.price);
 
     }
 }
