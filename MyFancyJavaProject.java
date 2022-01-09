6ed1f1f8a6c9e69a3a8eb7ed703797d00b84a890
muansuiu <mnezami173053@bscse.uiu.ac.bd>
2022-01-09 11:47:36 +0600

    A new variable called ram added

--- MyFancyJavaProject.java
+++ MyFancyJavaProject.java
@@ -2,18 +2,20 @@ public class MyFancyJavaProject {
     String phoneModel;
     String phoneBrand;
     double price;
+    int ram;
 
-    MyFancyJavaProject(String phoneModel,String phoneBrand, double price)
+    MyFancyJavaProject(String phoneModel,String phoneBrand, double price,int  ram)
     {
         this.phoneModel = phoneModel;
         this.phoneBrand = phoneBrand;
         this.price = price;
+        this.ram = ram;
     }
 
     public static void main(String[] args) {
 
-        MyFancyJavaProject newPhone = new MyFancyJavaProject("Galaxy S21 Ultra", "Samsung",110000);
-        System.out.println(newPhone.phoneModel + " " + newPhone.phoneBrand + " "+ newPhone.price);
+        MyFancyJavaProject newPhone = new MyFancyJavaProject("Galaxy S21 Ultra", "Samsung",110000,4);
+        System.out.println(newPhone.phoneModel + " " + newPhone.phoneBrand + " "+ newPhone.price + " " + newPhone.ram);
 
     }
 }
