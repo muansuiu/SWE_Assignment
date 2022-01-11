03ec8cc21511bf8714fbfa3edcc327c2a08ace0b
javatcodein <mbiswas173036@bscse.uiu.ac.bd>
2022-01-11 23:57:38 +0600

    branchXY merge with Y

--- MyFancyJavaProject.java
+++ MyFancyJavaProject.java
@@ -2,12 +2,16 @@ public class MyFancyJavaProject {
     String phoneModel;
     String phoneBrand;
     double price;
+    int ram;
+    int Rom;
 
-    MyFancyJavaProject(String phoneModel,String phoneBrand, double price)
+    MyFancyJavaProject(String phoneModel,String phoneBrand, double price,int  ram)
     {
         this.phoneModel = phoneModel;
         this.phoneBrand = phoneBrand;
         this.price = price;
+        this.ram = ram;
+        this.Rom = Rom;
     }
 
   MyProject(String pcModel,String pcBrand, double price)
@@ -19,8 +23,8 @@ public class MyFancyJavaProject {
 
     public static void main(String[] args) {
 
-        MyFancyJavaProject newPhone = new MyFancyJavaProject("Galaxy S21 Ultra", "Samsung",110000);
-        System.out.println(newPhone.phoneModel + " " + newPhone.phoneBrand + " "+ newPhone.price);
+        MyFancyJavaProject newPhone = new MyFancyJavaProject("Galaxy S21 Ultra", "Samsung",110000,4);
+        System.out.println(newPhone.phoneModel + " " + newPhone.phoneBrand + " "+ newPhone.price + " " + newPhone.ram);
 
         MyFancyJavaProject newPC = new MyProject("thinkpad_genaration_5", "Lenovo",500000);
         System.out.println(newPC.pcModel + " " + newPC.pcBrand + " "+ newPC.price);
